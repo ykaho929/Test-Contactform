2bc0fabdf646b23adc48470e0ba05b0d8a565e58
ykaho <h12.929.kaho@gmail.com>
2024-11-08 12:36:10 +0900

    問合せルーティング

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+    public function confirm(Request $request)
+    {
+        $contact = $request->only(['category_id','first_name','last_name','gender','email','tel','adress','building','detail']);
+        return view('confirm', compact('contact'));
+    }
+    public function store()
+    {
+        $contact = $request->only(['category_id','first_name','last_name','gender','email','tel','adress','building','detail']);
+        Contact::create($contact);
+        return view('thanks');
+    }
 }
