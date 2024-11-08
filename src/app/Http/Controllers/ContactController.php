027221023487c411100427d8da496f9c7538f15c
ykaho <h12.929.kaho@gmail.com>
2024-11-08 13:34:44 +0900

    ユーザー認証機能

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -11,15 +11,30 @@ class ContactController extends Controller
     {
         return view('index');
     }
+
     public function confirm(Request $request)
     {
         $contact = $request->only(['category_id','first_name','last_name','gender','email','tel','adress','building','detail']);
         return view('confirm', compact('contact'));
     }
+
     public function store()
     {
         $contact = $request->only(['category_id','first_name','last_name','gender','email','tel','adress','building','detail']);
         Contact::create($contact);
         return view('thanks');
     }
+
+    // public function process(Request $request)
+    // {
+    //     $action = $request->get('action', 'back');
+    //     $input = $request->except('action');
+    //     if($action === 'submit') {
+    //         return view('complete');
+    //         } else {
+    //         return redirect()->action('ContactController@form')
+    //         ->withInput($input);
+    //         }
+
+    // }
 }
