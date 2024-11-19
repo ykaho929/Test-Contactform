2a401f14ae2ee990c7dc8bfe0f9c7c554e09a112
ykaho <h12.929.kaho@gmail.com>
2024-11-19 14:58:44 +0900

    登録ログイン機能修正

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/login';
+    public const HOME = '/admin';
 
     /**
      * The controller namespace for the application.
@@ -58,6 +58,10 @@ class RouteServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
+
+         RateLimiter::for('web', function (Request $request) {
+            return Limit::perMinute(100)->by($request->ip());
         });
     }
 }
